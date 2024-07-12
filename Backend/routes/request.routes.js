ee6fed31f150104f5b2e221a68e1a29a0d938f88
SujalDhiman <sujaldhiman2003@gmail.com>
2024-07-12 18:22:40 +0530

    small bug fix

--- Backend/routes/request.routes.js
+++ Backend/routes/request.routes.js
@@ -7,8 +7,11 @@ const router=express.Router()
 
 
 router.route('/request').post(sendRequest)
+
 router.route('/inbox/:id').get(getInboxDetails)
+
 router.route('/accept/:userId/:friendId').get(acceptRequest)
+
 router.route('/showFriend/:userId').get(showFriends)
 
 export default router
